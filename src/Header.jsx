c9206fdb08ec578054f2b1d336d46704dad3ce47
M. KALEEMULLAH KHAN QURESHI <kaleemullahqurashi29@gmail.com>
2024-07-06 22:57:28 +0500

    add first class of react on github
    first work of react create a page in which i can add header footer and the main like food page  it is for practise

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+
+
+
+function Header(){
+   return( <header>
+        <h1>My Website</h1>
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+        </nav>
+        <hr />
+    </header>
+    );
+}
+
+export  default Header; 
